670392bae9468c850fb3301aaa53fde0491cd94f
sqler21c <sqler21c@gmail.com>
2017-03-03 15:28:39 +0900

    test

--- com.exam/thread/TestThread.java
+++ com.exam/thread/TestThread.java
@@ -1,6 +1,29 @@
 package com.exam.thread;
 
 
-public class TestThread extends Thread{
 
+public class TestThread extends Thread{
+  int seq;
+  public TestThread(int seq) {
+    this.seq = seq;
+  }
+
+  public void run(){
+    System.out.System.out.println(this.seq + " thread start");
+
+    try{
+      Thread.sleep(1000);
+    }catch{InterruptedException e){
+      e.printStackTrace();
+    }
+
+  }
+
+  public void static main(String[] args) {
+    for(int i=0; i<10; i++){
+      Thread t = new TestThread(i);
+      i.start();
+    }
+    System.out.println("Main ended....  ");
+  }
 }
